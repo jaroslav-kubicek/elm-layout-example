60fee7bc8a651fcd96ea156e6225628d0f746788
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2017-07-16 21:59:59 +0200

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, img)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        []
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
